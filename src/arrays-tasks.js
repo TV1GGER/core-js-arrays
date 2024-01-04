53fd29786e8d355836aae11399ff365ee92ba0d5
Perahudau Dzianis <tvigger@yandex.ru>
2024-01-04 22:53:58 +0300

    feat: add code for calculateBalance function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -364,8 +364,16 @@ function selectMany(/* arr, childrenSelector */) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  let resultCount = 0;
+  function countItem(acc, item) {
+    resultCount += acc - item;
+  }
+  function everyItem(x) {
+    x.reduce(countItem);
+  }
+  arr.some(everyItem);
+  return resultCount;
 }
 
 /**
