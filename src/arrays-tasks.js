99fa8311895d922338dd4c3385d2aa168f380b48
Perahudau Dzianis <tvigger@yandex.ru>
2024-01-04 18:49:38 +0300

    feat: add code for findElement function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -74,8 +74,12 @@ function sumArrays(arr1, arr2) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  const searchNum = value;
+  if (arr.indexOf(searchNum) === -1) {
+    return -1;
+  }
+  return arr.indexOf(searchNum);
 }
 
 /**
