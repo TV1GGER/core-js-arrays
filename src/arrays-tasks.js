26be3c5f6bc8a01dc4739bcd0ca0db22080ef77e
Perahudau Dzianis <tvigger@yandex.ru>
2024-01-04 21:10:53 +0300

    feat: add code for isValueEqualsIndex function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -200,8 +200,11 @@ function isSameLength(arr) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  function searchHAveSameLangth(n) {
+    return n === arr.indexOf(n);
+  }
+  return arr.some(searchHAveSameLangth);
 }
 
 /**
