a052838029ee848ad37635633a2d8dc27ea22c4c
Perahudau Dzianis <tvigger@yandex.ru>
2024-01-05 14:21:11 +0300

    feat: add code for findCommonElements function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -569,8 +569,15 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const resultArr = [];
+  function checkItem(n) {
+    if (arr1.includes(n)) {
+      resultArr.push(n);
+    }
+  }
+  arr2.some(checkItem);
+  return resultArr;
 }
 
 /**
