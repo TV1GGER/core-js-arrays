00c94de975768aa049d4e9ff18a5454ec204a670
Perahudau Dzianis <tvigger@yandex.ru>
2024-01-05 13:51:55 +0300

    feat: add code for getIndicesOfOddNumbers function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -494,8 +494,15 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const resulrArr = [];
+  function returnIndexItem(n) {
+    if (n % 2 === 1) {
+      resulrArr.push(numbers.indexOf(n));
+    }
+  }
+  numbers.some(returnIndexItem);
+  return resulrArr;
 }
 
 /**
